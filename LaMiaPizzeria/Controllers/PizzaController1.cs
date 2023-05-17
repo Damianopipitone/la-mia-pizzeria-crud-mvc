cdda3a1080feb5c5228b5d1386bc19161c54a40e
Damianopipitone <damianopipitone33@gmail.com>
2023-05-17 17:15:50 +0200

    debug

--- LaMiaPizzeria/Controllers/PizzaController1.cs
+++ LaMiaPizzeria/Controllers/PizzaController1.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaMiaPizzeria.DataBase;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers
 {
     public class PizzaController1 : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult NewIndex()
+        {   
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<PizzaModel> pizze = db.Pizze.ToList();
+            }
+            return View(pizze);
         }
     }
 }
