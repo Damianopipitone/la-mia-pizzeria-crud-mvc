c2ee6464a24ac8c5b7b927be5e8a69d2762855af
Damianopipitone <damianopipitone33@gmail.com>
2023-05-17 14:55:29 +0200

    aggiunto modello pizzaModel e  pizzacontroller - neverGonnaGiveYouUp

---
+++ LaMiaPizzeria/Models/PizzaModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeria.Models
+{
+    public class PizzaModel
+    {
+        public int id { get; set; }
+        
+        [MaxLength(40)]
+        public string Name { get; set; }
+        
+        [MaxLength(100)]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        
+        [MaxLength(300)]
+        public string imgSource { get; set; }
+        public float Price { get; set; }
+
+
+    }
+}
